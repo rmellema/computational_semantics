19e1a185104f50409968205f6b233ffb4dcd4f29
rmellema <rene.mellema@gmail.com>
2017-01-26 19:43:48 +0100

    Added a main predicate for searching through the models

--- prolog/search.pl
+++ prolog/search.pl
@@ -1,6 +1,9 @@
 :- use_module(library(apply)).
 :- use_module(library(lists)).
+
+:- [utils].
 :- [model_checker].
+:- [semdcg].
 
 make_assingment([], [], []).
 make_assingment([V | Vs], [X | Xs], [g(V, X) | G]) :- 
@@ -38,3 +41,41 @@ relevance(Query, Model, Relevance) :-
 	maplist(relevance_calculation(Model), Domains, Relevances),
 	max_list(Relevances, Relevance).
 
+read_query(Stream, Query) :-
+	read(Stream, Sentence),
+	s(Sem, Sentence, []),
+	betaConvert(Sem, Query).
+
+filter_models(_, _, [], []).
+filter_models(Wff, Vars, [path(P, M) | AllModels], [path(P, M) | Models]) :-
+	g(Vars, M, G),
+	satisfy(Wff, M, G, true), !,
+	filter_models(Wff, Vars, AllModels, Models).
+filter_models(Wff, Vars, [_ | AllModels], Models) :-
+	filter_models(Wff, Vars, AllModels, Models).
+filter_models(Query, AllModels, Models) :-
+	wff(Query, Wff, [], Vars),
+	filter_models(Wff, Vars, AllModels, Models).
+
+rel_list_to_paths([], []).
+rel_list_to_paths([(_, P, _) | T], [P | PT]) :- rel_list_to_paths(T, PT).
+
+sort_models(Query, Models, SortedModels) :-
+	findall((R, P, M), (member(path(P, M), Models), relevance(Query, M, R)), MR),
+	sort(1, @>=, MR, SMR),
+	rel_list_to_paths(SMR, SortedModels).
+
+search(Stream, Models, SortedPaths) :-
+	read_query(Stream, Query),
+	filter_models(Query, Models, TrueModels),
+	sort_models(Query, TrueModels, SortedPaths).
+search(Models, RelMods) :-
+	search(user_input, Models, RelMods).
+
+main(Limit) :-
+	load_directory('./data/', Models),
+	search(Models, RelPaths),
+	take(Limit, RelPaths, First),
+	maplist(change_ext('mod', 'jpg'), First, ImgPaths),
+	write_list(ImgPaths).
+main() :- main(5).
