ef238b6a21a4039a8f7019f48ec0248bc6776a2c
rmellema <rene.mellema@gmail.com>
2017-01-26 21:01:52 +0100

    Made the searcher code a bit neater

--- scripts/searcher.py
+++ scripts/searcher.py
@@ -4,7 +4,7 @@ from nltk import pos_tag, word_tokenize
 from nltk.corpus import words
 from nltk.metrics import edit_distance
 import shlex
-from subprocess import run, PIPE
+from subprocess import run, PIPE, STDOUT
 import sys
 
 def spell_check(word):
@@ -46,17 +46,11 @@ def to_latex(printer, paths):
         printer("\\includegraphics[width=120pt,height=90pt]{{{}}}".format(path))
     printer(r"\end{document}")
 
-def main(options):
-    line = input("> ")
-    correct_line = correct_sentence(word_tokenize(line.lower()))
-    grim_search = os.path.join(options.grim, "prolog", "search.pl")
-    result = run(
-            ['swipl', '-g', "[\'{}\'], main({}), halt." \
-                    .format(grim_search, options.number)],
-            input  = str(correct_line) + '.', encoding = 'utf-8',
-            stdout = PIPE)
+def output_results(options, paths):
+    if paths[0] == '' or paths[0] == '% halt':
+        sys.stderr.write("No results found.\n")
+        return
     if options.latex:
-        paths = result.stdout.strip().split('\n')
         if options.latex == '-':
             to_latex(print, paths)
         else:
@@ -67,14 +61,38 @@ def main(options):
                     f.write('\n')
                 to_latex(writer, paths)
     if options.display:
-        paths = result.stdout.strip().split('\n')
         command = shlex.split(options.display)
         for path in paths:
             c = command[:]
             c.append(path)
             run(c)
     else:
-        print(result.stdout.strip())
+        for path in paths:
+            print(path)
+
+def main_loop(options, grim_search, command):
+    try:
+        while True:
+            single_run(options, grim_search, command)
+    except EOFError:
+        print()
+
+def single_run(options, grim_search, command):
+    line = input("> ")
+    correct_line = correct_sentence(word_tokenize(line.lower()))
+    result = run(command,
+            input  = str(correct_line) + '.', encoding = 'utf-8',
+            stdout = PIPE, stderr= STDOUT)
+    output_results(options, result.stdout.strip().split('\n'))
+
+def main(options):
+    grim_search = os.path.join(options.grim, "prolog", "search.pl")
+    command = ['swipl', '-g', "[\'{}\'], main({}), halt." \
+                        .format(grim_search, options.number)]
+    if options.interactive:
+        main_loop(options, grim_search, command)
+    else:
+        single_run(options, grim_search, command)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -86,9 +104,13 @@ if __name__ == '__main__':
             default = ".")
     parser.add_argument("-l", "--latex",
             help = "Write the output to a latex file. If '-' or no file is" 
-                   "given, stdout is used for output",
+                   "given, stdout is used for output. If multiple queries are"
+                   "given in a row, the file gets overwritten after each query.",
             nargs = '?',
             const = '-')
+    parser.add_argument("-i", "--interactive",
+            action = "store_true",
+            help = "Process multiple queries in a row")
     parser.add_argument("-d", "--display",
             help = "A command used to display the found images. The command" 
                    "is called for each image and appended to the end of the"
